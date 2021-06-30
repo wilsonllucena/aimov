bb8e4345274efca4d175625f4d22835b2b74c80a
Wilson Lucena <wilson.lucena@datainfo.inf.br>
2021-06-29 23:55:41 -0300

    Criação de novos usuários

--- frontend/src/components/ToastContainer/styles.ts
+++ frontend/src/components/ToastContainer/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
   position: absolute;
   right: 0;
   top: 0;
+  z-index: 10;
   padding: 30px;
   overflow: hidden;
 `;
--- frontend/src/views/admin/users/ListUsers.tsx
+++ frontend/src/views/admin/users/ListUsers.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
 import CardTable from "components/Cards/CardTable.js";
-
-// import api from "../../services/api";
+import api from "services/api";
 
 interface ListUserProps {
   id: string;
   name: string;
   full_name: string;
+  email:string;
+  cep: string;
+  document: string;
+  address:string;
+  address_complement: string;
+  is_admin: string;
+  active:string;
 }
 const ListUser : React.FC = () => {
   const [users, setUsers] = useState<ListUserProps[]>([]);
 
-  // useEffect(() => {
-  //   api.get("/schedules")
-  //   .then((response) => {
-  //     setSchedules(response.data);
-  //   }, []);
-  // });
-
-  // console.log("Eventos: ", schedules);
+  useEffect(() => {
+    api.get("/users")
+    .then((response) => {
+      setUsers(response.data);
+    });
+  },[]);
 
   return (
     <>
@@ -31,7 +35,13 @@ const ListUser : React.FC = () => {
                   Nome
                 </th>
                 <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700">
-                  Nome Completo
+                  Documento
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700">
+                  E-mail
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700">
+                  Administrador
                 </th>
                 <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700">
                   Ações
@@ -42,13 +52,19 @@ const ListUser : React.FC = () => {
               {users.map((user) => (
                 <tr key={user.id}>
                   <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                    {user.name}
+                    {user.full_name}
                   </th>
                   <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    {user.full_name}
+                    {user.document}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.email}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.is_admin ? 'SIM' : 'NÃO'}
                   </td>
                   <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                    
+
                   </td>
                 </tr>
               ))}
