475bc488f9ddf0eec8db14abe45efceb817a5b42
Wilson Lucena <wilson.lucena@datainfo.inf.br>
2021-07-11 19:53:16 -0300

    Identacao de arquivos

--- backend/src/modules/accounts/repositories/IUsersRepository.ts
+++ backend/src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,14 +1,12 @@
-import { ICreateUsersDTO } from "../dtos/ICreateUsersDTO";
-import User from "../entities/User";
-
-
+import { ICreateUsersDTO } from '../dtos/ICreateUsersDTO';
+import User from '../entities/User';
 interface IUsersRepository {
-  create(data: ICreateUsersDTO): Promise<User | void>
-  findByEmail(email: string): Promise<User | undefined>
-  findByDocument(document: string): Promise<User | undefined>
-  findById(id: string): Promise<User | undefined>
+  create(data: ICreateUsersDTO): Promise<User | void>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findByDocument(document: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
   list(): Promise<User[]>;
   save(user: User): Promise<User>;
 }
 
-export { IUsersRepository, ICreateUsersDTO }
+export { IUsersRepository, ICreateUsersDTO };
