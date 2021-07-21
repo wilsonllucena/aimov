5eec99163c051cdb0fb94e34f72daaa60012fbae
Wilson Lucena <wilsonllucena@gmail.com>
2021-07-21 01:08:36 -0300

    Funcionalidade sair do sistema

--- frontend/src/components/Header/Profile.tsx
+++ frontend/src/components/Header/Profile.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import {Box, Flex, Text, Avatar} from '@chakra-ui/react'
-import { useAuth } from '../../hooks/AuthContext';
+import React from "react";
+import {
+	Box,
+	Flex,
+	Text,
+	Avatar,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+} from "@chakra-ui/react";
+import { useAuth } from "../../hooks/AuthContext";
 
 interface ProfileProps {
-  showProfileData?: boolean;
+	showProfileData?: boolean;
 }
-const Profile: React.FC<ProfileProps> = ({showProfileData}) => {
-const { user } = useAuth();
+const Profile: React.FC<ProfileProps> = ({ showProfileData }) => {
+	const { user } = useAuth();
+	const { signOut } = useAuth();
 
-  return (
-    <Flex align="center">
-      { showProfileData && 
-        <Box mr="4" textAlign="right">
-        <Text>Olá! {user.name}</Text>
-        <Text color="gray.300" fontSize="small">{user.email}</Text>
-      </Box>
-      }
-
-    <Avatar size="md"  name={user.full_name} src={user.avatar_url}/>
-  </Flex>
-  );
-}
+	return (
+		<Flex align="center">
+			{showProfileData && (
+				<Box mr="4" textAlign="right">
+					<Text>Olá! {user.name}</Text>
+					<Text color="gray.300" fontSize="small">
+						{user.email}
+					</Text>
+				</Box>
+			)}
+			<Menu>
+				<MenuButton>
+					<Avatar
+						size="md"
+						name={user.full_name}
+						src={user.avatar_url}
+					/>
+				</MenuButton>
+				<MenuList>
+					<MenuItem onClick={signOut} color="black">Sair</MenuItem>
+				</MenuList>
+			</Menu>
+		</Flex>
+	);
+};
 
 export default Profile;
